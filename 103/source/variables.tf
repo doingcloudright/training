4d5f884545f29313ff3c002e40d27657a1a26e50
Maarten van der Hoef <maarten@vanderhoef.eu>
2018-10-18 11:49:43 +0200

    Update variables.tf

--- 103/source/variables.tf
+++ 103/source/variables.tf
@@ -11,13 +11,16 @@ variable "network" {
 }
 
 variable "availability_zones" {
+  type = "list"
   description = "Which availability zones do we map our subnets to"
 }
 
 variable "public_subnets" {
+  type = "list"
   description = "CIDRs of the public subnet"
 }
 
 variable "private_subnets" {
+  type = "list"
   description = "CIDRs of the private subnet"
 }
