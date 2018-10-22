33ae132d3cfb42febd3f7dc56bebadbf3e3e4a2c
maartenvanderhoef <maarten@blinkist.com>
2018-10-22 11:40:41 +0200

    No IAM yet

--- 106/source/cattle-webservers/main.tf
+++ 106/source/cattle-webservers/main.tf
@@ -1,67 +1,3 @@
-# We create the bucket here
-resource "aws_s3_bucket" "bucket" {
-  bucket_prefix = "${var.name}"
-
-  # https://docs.aws.amazon.com/AmazonS3/latest/dev/acl-overview.html#canned-acl
-  acl = "private"
-
-  force_destroy = true // Allows us to remove the bucket easily at the end of the tutorial
-
-  # We keep versioning disabled, for production use, enabling versioning is best practice
-  # versioning {
-  #  enabled = true
-  # }
-}
-
-# Policy Document to allow S3 Read-Write Access to given paths
-data "aws_iam_policy_document" "bucket_s3_all_permissions" {
-  # We need to give access to list the buckets, in this case, list buckets will only return one bucket
-  statement {
-    effect    = "Allow"
-    actions   = ["s3:ListBucket"]
-    resources = ["${aws_s3_bucket.bucket.arn}"]
-  }
-
-  # s3:* mean that all s3 actions will be allowed
-  statement {
-    effect    = "Allow"
-    actions   = ["s3:*"]
-    resources = ["${aws_s3_bucket.bucket.arn}/*"]
-  }
-}
-
-# Trust Policy
-data "aws_iam_policy_document" "ec2_instance_trust_policy" {
-  statement {
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
-
-    principals = {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
-
-# Creation of the role
-resource "aws_iam_role" "ec2_s3_access_role" {
-  name               = "${var.name}-ec2-bucket-access"
-  assume_role_policy = "${data.aws_iam_policy_document.ec2_instance_trust_policy.json}"
-}
-
-resource "aws_iam_role_policy" "this" {
-  # Name is optional, please test with and without
-  # name = "this_is_not_mandatory"
-  role = "${aws_iam_role.ec2_s3_access_role.id}"
-
-  policy = "${data.aws_iam_policy_document.bucket_s3_all_permissions.json}"
-}
-
-resource "aws_iam_instance_profile" "this" {
-  name = "${var.name}-instance-profile"
-  role = "${aws_iam_role.ec2_s3_access_role.id}"
-}
-
 ## Security Groups
 resource "aws_security_group" "loadbalancer" {
   vpc_id      = "${var.vpc_id}"
@@ -183,7 +119,6 @@ data "template_file" "init" {
 resource "aws_launch_configuration" "this" {
   image_id                    = "${data.aws_ami.bitnami.id}"
   instance_type               = "t2.micro"
-  iam_instance_profile        = "${aws_iam_instance_profile.this.arn}"
   security_groups             = ["${aws_security_group.instance.id}"]
   associate_public_ip_address = "${var.associate_public_ip_address}"
 
